f894718558507bca6cd3b868aaecb0be11fa1671
porostIslandzki <318632@office.umcs.pl>
2024-08-10 06:13:50 -0700

    my changes

---
+++ springcloud/receiver/src/main/java/com/first_chat/receiver/notification/Notification.java
@@ -0,0 +1,33 @@
+package com.first_chat.notification;
+
+import java.io.Serializable;
+
+public class Notification implements Serializable {
+    private String email;
+    private String title;
+    private String body;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}

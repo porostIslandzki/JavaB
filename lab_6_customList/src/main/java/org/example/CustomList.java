e5a0ae0c06b1fc5f546abf8160f27172b826e1d3
porostIslandzki <318632@office.umcs.pl>
2024-08-24 15:54:59 -0700

    changes added

---
+++ lab_6_customList/src/main/java/org/example/CustomList.java
@@ -0,0 +1,117 @@
+package org.example;
+
+import java.util.LinkedList;
+//Napisz klasę szablonową CustomList implementującą strukturę
+// listy jednokierunkowej ze wskaźnikami na początek i koniec.
+
+class Node<T> {
+    T data;
+    Node next;
+    Node(T d) {
+        this.data = d ;
+        this.next = null;
+    } //przechowuje dane oraz wskaźnik na kolejny element w liście
+}
+
+//Lista jednokierunkowa składa się z węzłów("Node"), gdzie każdy węzeł
+//zawiera: 'data' - przechowywaną wartość, 'next' - odniesienie do następnego węzła
+
+public class CustomList <T>  {
+    private Node<T> head; //wskaźnik na pierwszy węzeł
+    private Node<T> tail; //wskaźnik na ostatni węzeł
+
+    public CustomList(Node<T> head, Node<T> tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+
+    //void addLast(T value) - dodającą wartość na koniec listy,
+    public void addLast(T value){
+        Node<T> node = new Node<>(value); //tworzę nowy węzeł 'Node', który przechowuje
+        // wartość value
+        //wskaźnik next tego węzła jest domyślnie ustawiony na null,
+        //ponieważ nowy węzeł nie wskazuje jeszcze na żaden kolejny węzeł
+        if (head == null){
+            head = node;
+            tail = node; //jeśli lista jest pusta, zarówno head jak i tail
+            //są ustawione na nowo utworzony węzeł
+        }
+        else {
+            tail.next = node; //obecny ostatni węzeł wskazuje
+            //teraz na nowy węzeł
+            tail = node;
+            //następnie aktualizuję wskaźnik tail
+            //aby wskazywał na nowy węzeł
+        }
+    }
+
+    //T getLast() - zwracającą wartość z końca listy
+    public T getLast(){
+        if (tail == null){
+            return null;
+        } else {
+            return tail.data;
+        }
+    }
+
+    //void addFirst(T value) - dodającą wartość na początek listy
+    public void addFirst(T value){
+        Node<T> node = new Node<>(value);
+        if(head == null){
+            head = node;
+            tail = node;
+        } else {
+            node.next = head; //nowy węzeł staje się pierwszym węzłem
+            //a jego wskaźnik next wskazuje na poprzedni pierwszy węzeł head
+            head = node;
+        }
+    }
+
+    //T getFirst() - zwracającą wartość z początku listy
+    public T getFirst(){
+        if(head == null){
+            return null;
+        } else {
+            return head.data;
+        }
+    }
+
+    //T removeFirst() - zwracającą i usuwającą element z początku listy
+    public T removeFirst(){
+        if (head == null){
+            return null;
+        }
+        else {
+            T headValue = head.data;
+            head = head.next;
+            return headValue;
+        }
+    }
+
+    //T removeLast() - zwracającą i usuwającą element z końca listy
+    public T removeLast(){
+        if (head == null) { //Lista jest pusta
+            return null;
+        }
+
+        if(head == tail){ //Lista ma tylko jeden element
+            T value = head.data;
+            head = null;
+            tail = null;
+            return value;
+        }
+
+        //Lista ma więcej niż jeden el
+        Node<T> newTail = head;
+        while (newTail.next != tail) { //szukam przedostatniego elementu
+            newTail = newTail.next;
+        }
+        T value = tail.data;
+        tail = newTail; //Przedostatni element staje się ostatnim
+        tail.next = null; //usuwam referencję do poprzedniego ostatniego
+
+        return value;
+    }
+
+
+}

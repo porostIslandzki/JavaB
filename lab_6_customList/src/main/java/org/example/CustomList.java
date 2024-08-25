879106dc358b4ab60fbed588e51f798ae0ffa0fa
porostIslandzki <318632@office.umcs.pl>
2024-08-25 12:35:41 -0700

    changes added

--- lab_6_customList/src/main/java/org/example/CustomList.java
+++ lab_6_customList/src/main/java/org/example/CustomList.java
@@ -1,7 +1,10 @@
 package org.example;
 
-import java.util.AbstractList;
-import java.util.LinkedList;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 //Napisz klasę szablonową CustomList implementującą strukturę
 // listy jednokierunkowej ze wskaźnikami na początek i koniec.
 
@@ -24,7 +27,7 @@ public class CustomList <T> extends AbstractList<T> {
     public boolean add(T t) { // nadpisz metode: ma działać tak samo jak addLast
         //i zwracać prawdę
         Node<T> addNode = new Node<>(t);
-        if(head == null){
+        if (head == null) {
             head = addNode;
             tail = addNode;
         } else {
@@ -49,15 +52,15 @@ public class CustomList <T> extends AbstractList<T> {
     @Override
     public T get(int index) { //zwraca wartość w węźle o podanym indeksie
         //rzucimy wyjątkami, kiedy: index będzie ujemny, index będzie większy od zakresu listy
-        if(index < 0) {
+        if (index < 0) {
             throw new IndexOutOfBoundsException("Index cannot be negative: " + index);
         }
 
         int counter = 0;
         Node<T> countingNode = head;
 
-        while (countingNode != null){
-            if(counter == index) {
+        while (countingNode != null) {
+            if (counter == index) {
                 return countingNode.data;
             }
             counter++;
@@ -75,18 +78,22 @@ public class CustomList <T> extends AbstractList<T> {
         this.tail = tail;
     }
 
+    public CustomList() {
+        this.head = null;
+        this.tail = null;
+    }
+
     //void addLast(T value) - dodającą wartość na koniec listy,
-    public void addLast(T value){
+    public void addLast(T value) {
         Node<T> node = new Node<>(value); //tworzę nowy węzeł 'Node', który przechowuje
         // wartość value
         //wskaźnik next tego węzła jest domyślnie ustawiony na null,
         //ponieważ nowy węzeł nie wskazuje jeszcze na żaden kolejny węzeł
-        if (head == null){
+        if (head == null) {
             head = node;
             tail = node; //jeśli lista jest pusta, zarówno head jak i tail
             //są ustawione na nowo utworzony węzeł
-        }
-        else {
+        } else {
             tail.next = node; //obecny ostatni węzeł wskazuje
             //teraz na nowy węzeł
             tail = node;
@@ -96,8 +103,8 @@ public class CustomList <T> extends AbstractList<T> {
     }
 
     //T getLast() - zwracającą wartość z końca listy
-    public T getLast(){
-        if (tail == null){
+    public T getLast() {
+        if (tail == null) {
             return null;
         } else {
             return tail.data;
@@ -105,9 +112,9 @@ public class CustomList <T> extends AbstractList<T> {
     }
 
     //void addFirst(T value) - dodającą wartość na początek listy
-    public void addFirst(T value){
+    public void addFirst(T value) {
         Node<T> node = new Node<>(value);
-        if(head == null){
+        if (head == null) {
             head = node;
             tail = node;
         } else {
@@ -118,8 +125,8 @@ public class CustomList <T> extends AbstractList<T> {
     }
 
     //T getFirst() - zwracającą wartość z początku listy
-    public T getFirst(){
-        if(head == null){
+    public T getFirst() {
+        if (head == null) {
             return null;
         } else {
             return head.data;
@@ -127,11 +134,10 @@ public class CustomList <T> extends AbstractList<T> {
     }
 
     //T removeFirst() - zwracającą i usuwającą element z początku listy
-    public T removeFirst(){
-        if (head == null){
+    public T removeFirst() {
+        if (head == null) {
             return null;
-        }
-        else {
+        } else {
             T headValue = head.data;
             head = head.next;
             return headValue;
@@ -139,12 +145,12 @@ public class CustomList <T> extends AbstractList<T> {
     }
 
     //T removeLast() - zwracającą i usuwającą element z końca listy
-    public T removeLast(){
+    public T removeLast() {
         if (head == null) { //Lista jest pusta
             return null;
         }
 
-        if(head == tail){ //Lista ma tylko jeden element
+        if (head == tail) { //Lista ma tylko jeden element
             T value = head.data;
             head = null;
             tail = null;
@@ -166,7 +172,122 @@ public class CustomList <T> extends AbstractList<T> {
     //Napisz metodę: Iterator<T> iterator() -
     // zwracającą iterator do listy. Zdefiniuj w niej iterator
 
-    
+    public Iterator<T> iterator() {
+        return new CustomIterator();
+    }
 
+    //Klasa wewnętrzna implementująca interfejs Iterator<T>
 
+    private class CustomIterator implements Iterator<T> {
+
+        private Node<T> currentNode;
+
+        public CustomIterator() {
+            this.currentNode = head; //Sortujemy od początku listy
+        }
+
+        @Override
+        public boolean hasNext() {
+            return currentNode != null; //oznacza, że można jeszcze
+            //pobrać element
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            } else {
+                T data = currentNode.data;
+                currentNode = currentNode.next;
+                return data;
+            }
+        }
+
+        //Stream<T> stream() - zwracającą strumień z zawartością listy.
+        public Stream<T> stream() {
+            return StreamSupport.stream(
+                    Spliterators.spliteratorUnknownSize(iterator(), 0), false);
+        }
+
+    }
+
+    //Napisz statyczną metodę szablonową, która przyjmie jako
+    // parametry Listę obiektów typu szablonowego T oraz obiekt
+    // Class. Metoda powinna zwrócić listę obiektów, które należą do wskazanej klasy.
+    //Następnie zmodyfikuj metodę tak, aby filtrowała obiekty, które dziedziczą
+    // (bezpośrednio lub pośrednio) po wskazanej klasie.
+
+    //metoda szablonowa to metoda w klasie bazowej
+    //która definiuje szkic algorytmu
+    //pozostawiając szczegóły implementacji niektórych kroków do
+    //zdefiniowania w klasach pochodnych
+
+    protected CustomList<T> belongsToClass(Class<?> oClass) {
+        CustomList<T> filteredList = new CustomList<>();
+
+        Node<T> currentNode = head; //iteruję przy pomocy currentNode
+        //zaczynając od head
+        while (currentNode != null) {
+            if (oClass.isInstance(currentNode.data)) { //metoda ta sprawdza
+                //czy dany obiekt jest instancją zadanej klasy lub jej podklasy
+                filteredList.add(currentNode.data);
+            }
+            currentNode = currentNode.next;
+        }
+
+        return filteredList;
+    }
+
+    //Napisz predykat, który porówna, czy testowana zmienna znajduje
+    // się w otwartym przedziale, zdefiniowanym jego granicami.
+    //Korzystając z niego napisz metodę statyczną, która
+    // dla listy oraz granic
+    // zakresu danych typem szablonowym zwróci liczbę elementów
+    // w tej liście, spełniających warunek predykatu.
+
+    public class RangeUtils {
+
+        // Predykat sprawdzający, czy wartość znajduje się w otwartym przedziale (minValue, maxValue)
+        public static <T extends Comparable<T>> Predicate<T> isWithinRange(T minValue, T maxValue) {
+            return value -> value.compareTo(minValue) > 0 && value.compareTo(maxValue) < 0;
+        }
+
+        // Metoda zliczająca elementy w liście, które spełniają warunek predykatu
+        public static <T extends Comparable<T>> long countWithinRange(List<T> list, T minValue, T maxValue) {
+            Predicate<T> rangePredicate = isWithinRange(minValue, maxValue);
+            return list.stream().filter(rangePredicate).count();
+        }
+    }
+
+    //Napisz komparator, który porówna dwie kolekcje pod względem
+    //liczby ich elementów. Następnie zmodyfikuj go tak,
+    // aby przyjmował wyłącznie kolekcje liczb i porównywał
+    // je pod względem ich sumy.
+
+    public class CollectionsComparator implements Comparator<Collection<? extends Number>> {
+        @Override
+        public int compare(Collection<? extends Number> c1, Collection<? extends Number> c2) {
+            double sum1 = 0;
+            double sum2 = 0;
+            Iterator<? extends Number> iterator1 = c1.iterator();
+            while (iterator1.hasNext()){
+                sum1 = sum1 + iterator1.next().doubleValue();
+            }
+            Iterator<? extends Number> iterator2 = c2.iterator();
+            while (iterator2.hasNext()){
+                sum2 = sum2 + iterator2.next().doubleValue();
+            }
+            return Double.compare(sum1, sum2);
+        }
+
+    }
+
+    //chat gpt znów się drze że da się zrobić prościej
+    //w metodzie compare:
+    // for(Number num : c1) {
+    // sum1 += num.doubleValue();
+    //}
+    // for(Number num : c2) {
+    // sum2 += num.doubleValue();
+    //}
 }
